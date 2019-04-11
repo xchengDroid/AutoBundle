851981a6bc84cec89c6c41efc56995b3486692a8
chengxin <chengx@hlsteel.com>
2019-04-11 15:26:19 +0800

    添加bundleFlag 标记对应的方法

---
+++ autobundle-annotation/src/main/java/autobundle/annotation/BundleFlag.java
@@ -0,0 +1,26 @@
+package autobundle.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Bind a method  for the specified value. The field will automatically be cast to the field
+ * type.
+ */
+@Retention(RUNTIME)
+@Target({METHOD})
+public @interface BundleFlag {
+    /**
+     * mark the bundle
+     */
+    int value();
+
+    /**
+     * Description of the bundle
+     */
+    String desc() default "";
+
+}
