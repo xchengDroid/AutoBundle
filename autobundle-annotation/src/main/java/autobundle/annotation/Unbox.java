4c4e5e793a1d2aac08451451485c8c354ed4aaea
chengxin <chengx@hlsteel.com>
2019-04-17 10:26:04 +0800

    修改为class

--- autobundle-annotation/src/main/java/autobundle/annotation/Unbox.java
+++ autobundle-annotation/src/main/java/autobundle/annotation/Unbox.java
@@ -4,13 +4,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Bind a field  for the specified value. The field will automatically be cast to the field
  * type. TypeName#box unbox
  */
-@Retention(RUNTIME)
+@Retention(CLASS)
 @Target(FIELD)
 public @interface Unbox {
     /**
